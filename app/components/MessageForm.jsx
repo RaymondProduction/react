de9d1dbd6761b1a1144e924a9ee7b0a743c3490c
Raymond Perminov <raymondproduction@ukr.net>
2018-01-24 23:57:44 +0200

    Added validation (with problem)

--- app/components/MessageForm.jsx
+++ app/components/MessageForm.jsx
@@ -2,35 +2,57 @@ var React = require('react');
 class MessageForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {message: ""};
+    var message = props.message;
+    var messageIsValid = this.validateMessage(message);
+    var title = props.title;
+    var titleIsValid = this.validateMessage(title);
 
-    this.onChange = this.onChange.bind(this);
+    this.state = {message: message, title: title, messageValid: messageIsValid, titleValid : titleIsValid};
+
+    this.onChangeMessage = this.onChangeMessage.bind(this);
+    this.onChangeTitle = this.onChangeTitle.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-  onChange(e) {
+  validateMessage(message){
+    return message.length>2;
+  }
+  validateTitle(title){
+    return title.length >2;
+  }
+  onChangeMessage(e) {
     var val = e.target.value;
-    this.setState({message: val});
-}
-
+    var valid = this.validateMessage(val);
+    this.setState({message: val, messageValid: valid});
+  }
+  onChangeTitle(e) {
+    var val = e.target.value;
+    var valid = this.validateTitle(val);
+    this.setState({message: val, titleValid: valid});
+  }
   handleSubmit(e) {
     e.preventDefault();
-    alert("Message: " + this.state.message);
+    alert("Title: " + this.state.message+" Message: " + this.state.message);
   }
 
   render() {
+    // color border of the field  for input message
+    var messageColor = this.state.messageValid ? "green" : "red";
+    // color border of the field  for input title
+    var titleColor = this.state.titleValid ? "green" : "red";
+
     return (
         <form onSubmit={this.handleSubmit}>
             <p>
+                <label>Title:</label><br />
+                <input type="text" value={this.state.title} onChange={this.onChangeTitle} style ={{borderColor: titleColor}}/>
+            </p>
+            <p>
                 <label>Message:</label><br />
-                <input type="text" value={this.state.name} onChange={this.onChange}/>
+                <input type="text" value={this.state.message} onChange={this.onChangeMessage} style ={{borderColor: titleColor}}/>
             </p>
             <input type="submit" value="Send" />
         </form>
     );
   }
 }
-ReactDOM.render(
-    <MessageForm />,
-    document.getElementById("app")
-)
 module.exports = MessageForm;
