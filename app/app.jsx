17b7298cdbb7801cc82927bfa1a8b62d31d34c6f
Raymond Perminov <raymondproduction@ukr.net>
2018-01-25 04:56:01 +0200

    Added route system

--- app/app.jsx
+++ app/app.jsx
@@ -1,16 +1,34 @@
 var ReactDOM = require('react-dom');
 var React = require('react');
+
 var MessageForm = require('./components/MessageForm.jsx');
-/*
-const messageList = {
-    title: 'Main theme',
-    messages: [
-      {title: 'First news', text: 'Hi, It is test'},
-      {title: 'Second news', text: 'Ok, How do you do?'}
-    ]
+
+// using ES6 modules
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+class About extends React.Component{
+    render(){
+        return <h2>About website</h2>;
+    }
 }
-*/
+class NotFound extends React.Component{
+    render(){
+        return <h2>Resource is not found</h2>;
+    }
+}
+
+class Main extends React.Component{
+    render(){
+        return <h2>Main</h2>;
+    }
+}
 ReactDOM.render(
-    <MessageForm  message="" title=""/>,
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Main} />
+            <Route path="/about" component={About} />
+            <Route component={NotFound} />
+        </Switch>
+    </BrowserRouter>,
     document.getElementById("app")
 )
--- webpack.config.js
+++ webpack.config.js
@@ -18,5 +18,21 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+  devServer: {
+    historyApiFallback: true, // Without this parameter it will be 'Cannot GET /...'
+    /*
+    This isn't an issue with React Router, this occurs with any client-side routing for single page applications.
+    The reason this occurs is because whatever is serving your application doesn't know what to do with /plan,
+    it only exists on the client, so it gives you a 404.
+
+    The configuration depends on what you're serving your application with, but,
+    the gist of it is you'll need to send all paths that aren't static files to load index.html.
+
+    Apache -> mod_rewrite
+    Nginx -> rewrite
+    Webpack Dev Server -> historyApiFallback
+    Express/Koa/Hapi/etc. -> a wildcard route to your index.html
+    */
+  },
 }
