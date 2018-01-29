b2e981a2b24bc9dcd1ac3f5517a16908f31a5020
Raymond Perminov <raymondproduction@ukr.net>
2018-01-29 18:23:10 +0200

    Fix id

--- app/reducer.jsx
+++ app/reducer.jsx
@@ -1,14 +1,16 @@
 var Map = require("immutable").Map;
+var id;
 
 var reducer = function(state = Map(), action) {
   switch (action.type) {
     case "SET_STATE":
+        id = action.state.messages[action.state.messages.length-1].id+1;
         return state.merge(action.state);
     case "ADD_MESSAGE":
         return state.update("messages", (messages) => messages.push(new Map({
             title: action.message.title,
             text: action.message.text,
-            id: messages.size+1
+            id: id++,
           })));
 
     case "DELETE_MESSAGE":
