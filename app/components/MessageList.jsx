eb8c1fb6175654a5e6202aff6e46ed63f779025f
Raymond Perminov <raymondproduction@ukr.net>
2018-01-29 19:09:27 +0200

    Added filter for list of messages

--- app/components/MessageList.jsx
+++ app/components/MessageList.jsx
@@ -6,11 +6,26 @@ var Message = require('./Message.jsx');
 class MessageList extends React.Component {
   constructor(props){
       super(props);
+      this.state = { messages: this.props.messages};
+      this.filterList = this.filterList.bind(this);
   }
+  filterList(text){
+      var filteredList = this.props.messages.filter(function(item){
+          return item.get('text').toLowerCase().search(text.toLowerCase())!== -1;
+      });
+      // update state
+      this.setState({messages: filteredList});
+  }
+  // when change props, for example as add message
+  componentWillReceiveProps(props){
+      this.setState({ messages: props.messages})
+  }
+
   render(){
     var i=0;
     return <div>
-        {this.props.messages.map(item =>
+        <SearchPlugin filter={this.filterList}/>
+        {this.state.messages.map(item =>
           <Message  id={item.get('id')}
                     key={i++}
                     text={item.get('text')}
